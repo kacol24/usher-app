5a8876c8db1c07e7392e1d570a29ba9933cc9425
Kevin Chandra <kacol.bot@gmail.com>
2022-07-29 01:14:57 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Usher App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
