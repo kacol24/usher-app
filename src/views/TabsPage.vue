b4f26d3cc21509bb28cc119261441adcf7d88af5
Kevin Chandra <kacol.bot@gmail.com>
2022-07-30 01:28:19 +0700

    urql playground

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -21,12 +21,40 @@
 import { defineComponent } from 'vue';
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
+import {gql, useQuery} from "@urql/vue";
+
 
 export default defineComponent({
   name: 'TabsPage',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
   setup() {
+    const result = useQuery({
+      query: `
+        {
+          invitations {
+            data {
+              guest_code
+              name
+              phone
+              guests
+              seating {
+                name
+              }
+              attendance {
+                sequence
+                sequence_group
+                serial_number
+                created_at
+              }
+            }
+          }
+        }
+      `
+    });
+
     return {
+      result,
+
       ellipse,
       square,
       triangle,
