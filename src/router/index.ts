f4f05fbd0fe9137feebdcfe248ddbac31598378a
Kevin Chandra <kacol.bot@gmail.com>
2022-08-25 16:39:37 +0700

    remove unused file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,11 @@
 import {createRouter, createWebHistory} from '@ionic/vue-router';
 import {RouteRecordRaw} from 'vue-router';
 import GuestListTab from '@/views/GuestListTab.vue';
-import GuestCheckIn from '@/views/GuestCheckIn.vue';
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         component: GuestListTab
-    },
-    {
-        path: '/invitations/:guest_code',
-        name: 'invitations.show',
-        component: GuestCheckIn
     }
 ]
 
