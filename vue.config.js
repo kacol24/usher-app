73212a7e735990b0076c82e386052850b8947258
Kevin Chandra <kacol.bot@gmail.com>
2022-08-30 11:53:12 +0700

    vue add pwa

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pwa: {
+    name: 'Usher App',
+    themeColor: '#09232f',
+    msTileColor: '#09232f',
+    appleMobileWebAppCapable: 'yes',
+    manifestOptions: {
+      name: 'Usher App - loVINyouforeFER'
+    }
+  }
+}
