dfd395a24e4c4b7efdec449925be90da36dbf368
Kevin Chandra <kacol.bot@gmail.com>
2022-08-30 11:53:20 +0700

    capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.lovinyouforefer.usher',
   appName: 'Usher App',
   webDir: 'dist',
   bundledWebRuntime: false
