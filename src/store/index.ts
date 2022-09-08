7cf6f3e15fe3cacc07d1e91a974cc13cd2a7c6f8
Kevin Chandra <kacol.bot@gmail.com>
2022-09-09 00:58:56 +0700

    map usher list to show name of usher

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,17 @@ import {reactive} from 'vue';
 import {useLocalStorage} from '@vueuse/core/index';
 
 const state = reactive({
-  invitations: [],
-  isLoading: false,
-  sequenceGroup: useLocalStorage('sequence_group', '')
+    invitations: [],
+    isLoading: false,
+    sequenceGroup: useLocalStorage('sequence_group', ''),
+    usherList: {
+        'S': 'Sutrisno',
+        'Z': 'Zella',
+        'P': 'Peter',
+        'V': 'Verly'
+    }
 });
 
 export default {
-  state
+    state
 };
